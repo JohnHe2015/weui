db1e36dbfb2809ba66004b9e41420c429d6670ee
johnHe <xiaozhemu@163.com>
2019-01-01 22:48:06 +0800

    wx

--- index.js
+++ index.js
@@ -82,7 +82,10 @@ app.get('/register',(req,res,next)=>{
 
 app.post('/coupon',(req,res,next)=>{   //接收api传过来的coupon数据
     console.log('come in coupon');
+    console.log(req.body);
     console.log(req.body.data);
+    console.log(req.query);
+    console.log(req.params);
     let data = req.body.data ;
     console.log("获取到的coupon数据是: "+JSON.stringify(data));
     res.render('register.ejs',{
