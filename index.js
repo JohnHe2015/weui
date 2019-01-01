729da6a439f0d652e625355fbd6c3a012aa9d9f8
johnHe <xiaozhemu@163.com>
2019-01-01 18:36:16 +0800

    wx

--- index.js
+++ index.js
@@ -61,9 +61,11 @@ app.get('/register',(req,res,next)=>{
 
 
 app.post('/coupon',(req,res,next)=>{   //接收api传过来的coupon数据
+    console.log('come in coupon');
+    console.log(req.body);
     let data = req.body;
-    console.log("获取到的coupon数据是: "+JSON.stringify(data));
+    console.log("获取到的coupon数据是: "+data);
     res.render('register.ejs',{
-        data
+        data : JSON.parse(data)
     });
 })
