88a296c64e8aed3562290970a0988a958ba6b504
JohnHe2015 <xiaozhemu@163.com>
2019-01-03 17:01:00 +0800

    wx

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const request = require('request');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
+
 const app = express();
 app.listen(8082);
 
@@ -100,7 +101,7 @@ app.get('/coupon/:id',(req,res,next)=>{   //接收api传过来的coupon数据
         })
 });
 
-app.get('/coupon/detail',(req,res,next)=>{        //接收coupon.ejs的参数传递给detail页面
+app.get('/coupon/detail/:id/:type/:startTime/:endTime/:count/',(req,res,next)=>{        //接收coupon.ejs的参数传递给detail页面
     let {id,count,endTime,startTime,type} = req.query;
     console.log('come in /coupn/detail/..');
     let arr = [];
