24f271e8aa2d8e2fccf2bcdcc682dc2be7c8ca34
johnHe <xiaozhemu@163.com>
2019-01-04 21:44:44 +0800

    wx

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const request = require('request');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-
+const utils = require('./common/common')
 
 const app = express();
 app.listen(8082);
@@ -101,8 +101,8 @@ app.get('/coupon/:id',(req,res,next)=>{   //接收api传过来的coupon数据
         })
 });
 
-app.get('/coupon/detail/:id/:type/:startTime/:endTime/:count/',(req,res,next)=>{        //接收coupon.ejs的参数传递给detail页面
-    let {id,count,endTime,startTime,type} = req.params;
+app.get('/coupon/detail/:id/:type/:startTime/:endTime/:count/:rate/',(req,res,next)=>{        //接收coupon.ejs的参数传递给detail页面
+    let {id,count,endTime,startTime,type,rate} = req.params;
     var arr = [];
     let count_temp = parseInt(count);
     for(let i = 1;i <= count_temp ; i++)    //detail界面下拉框数组
@@ -112,13 +112,31 @@ app.get('/coupon/detail/:id/:type/:startTime/:endTime/:count/',(req,res,next)=>{
             value: i
         })
     }
+    let cusType;
+    if(type == "1")
+    {
+        cusType = "MUSEE CAFE 咖啡券";
+    }
+    else if (type == "2")
+    {
+        cusType = (rate*10)+"折拍摄券";
+    }
+    else if (type == "3")
+    {
+        cusType = (rate*10)+"折摄影券";
+    }
+    let _startTime = utils.getDate2(startTime);
+    let _endTime = utils.getDate2(endTime);
     res.render('couponDetail.ejs',{
         data : {
             id : id,
             arr : arr,
             type : type,
+            cusType : cusType,
             startTime : startTime,
             endTime : endTime,
+            _startTime : _startTime,
+            _endTime : _endTime,
             count : count_temp
         }   
     });
