3cb7ef15a557345d0a0cd9cc971a2ccb6eeb8fa6
johnHe <xiaozhemu@163.com>
2019-01-05 12:42:48 +0800

    wx

--- index.js
+++ index.js
@@ -141,7 +141,7 @@ app.get('/coupon/detail/:id/:type/:startTime/:endTime/:count/:rate/',(req,res,ne
     next();
 });
 
-app.get('/coupon/generateQR/:url',(req,res,next)=>{
+app.use('/coupon/generateQR/:url',(req,res,next)=>{
     console.log(req.query);
     console.log(req.params);
     let {imgsrc} = req.query;
